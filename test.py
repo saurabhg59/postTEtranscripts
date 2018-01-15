48295acde70362650de17a7033f7a1ebabe1791b
Saurabh Gulati <34722511+saurabhg59@users.noreply.github.com>
2018-01-14 19:04:11 -0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,52 @@
+#! /home/saurabhg59/anaconda2/bin/python
+
+import numpy as np
+import sys
+
+files={}
+geneValues={}
+teValues={}
+l1Values={}
+h2bValues={}
+
+H2Bindex = {"NM_001002916" : 0, "NM_170610" : 1,
+"NM_021062" : 2, "NM_003526" : 3,
+"NM_021063" : 4, "NM_138720" : 5,
+"NM_003523" : 6, "NM_003522" : 7,
+"NM_003518" : 8, "NM_003524" : 9,
+"NM_003525" : 10, "NM_021058" : 11,
+"NM_001312653" : 12, "NM_080593" : 13,
+"NM_003519" : 14, "NM_003521" : 15,
+"NM_003520" : 16, "NM_003527" : 17,
+"NM_003528" : 18}
+
+for filename in sys.argv[1:]:
+	lineCount=0
+	with open(filename,'r') as INPUT:
+		for line in INPUT:
+			line=line.strip()
+			if (lineCount==0):
+				a=line.split("\t")
+				a[1]=a[1][:-2]
+				a[2]=a[2][:-2]
+				files[a[1]]=a[2]
+				geneValues[a[1]]=[]
+				teValues[a[1]]=[]
+				h2bValues[a[1]]=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+				geneValues[a[2]]=[]
+				teValues[a[2]]=[]
+				h2bValues[a[2]]=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+				lineCount+=1
+			else:
+				line=line.strip()
+				b=line.split("\t")
+				if (":" in b[0]):
+					teValues[a[1]].append(int(b[1]))
+					teValues[a[2]].append(int(b[2]))
+				else:
+					geneValues[a[1]].append(int(b[1]))
+					geneValues[a[2]].append(int(b[2]))
+					for i in H2Bindex:
+						if i in b[0]:
+							h2bValues[a[1]][H2Bindex[i]]=int(b[1])
+							h2bValues[a[2]][H2Bindex[i]]=int(b[2])
