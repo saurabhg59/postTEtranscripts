b9cc879515eddaed497c352bca88444c3b143c26
Saurabh Gulati <saurabhg59@gmail.com>
2018-02-22 14:29:27 -0500

    removed outliers

--- test.R
+++ test.R
@@ -82,7 +82,7 @@ datafile$id<-factor(datafile$id,labels = c("Wild-Type L1","Mutant L1","Wild-Type
 #########################################################################################################################################################
 
 # plot1<-ggboxplot(datafile,x="id",y="value",color="black",fill="id",palette=c("royalblue1","orange1","royalblue4","orange4"),shape="id", xlab=FALSE, ylab="Log10 transformed expression levels", show.legend=FALSE)
-plot1<-ggboxplot(datafile,x="id",y="value",color="black",fill="id",palette=c("royalblue1","orange1","royalblue4","orange4"),shape="id", xlab=FALSE, ylab=FALSE, show.legend=FALSE)
+plot1<-ggboxplot(datafile,x="id",y="value",color="black",fill="id",palette=c("royalblue1","orange1","royalblue4","orange4"),shape="id", xlab=FALSE, ylab=FALSE, show.legend=FALSE,outlier.shape=NA)
 if(a$p.value>=0.0001 && a$p.value<=1){
 	plot1<-plot1+geom_text(aes(x=1.5 , y = 4, label = paste("P = ",round(a$p.value,digits=4),sep = ""),fontface="italic"), size = 2)+geom_segment(mapping=aes(x=1,y=3.8,xend=2,yend=3.8))+geom_segment(mapping=aes(x=1,y=3.8,xend=1,yend=3.5))+geom_segment(mapping=aes(x=2,y=3.8,xend=2,yend=3.5))
 }else if(a$p.value>0 && a$p.value<0.0001){ 
